ef842bb7957b3e50cc465b0285227a689d5a7a46
MarkusHackspacher <hackspacher@gmx.de>
2013-01-04 21:24:24 +0100

    change icon

--- lotto/lotto_gui1.py
+++ lotto/lotto_gui1.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Das Programm hat die Creative Commons by-sa Lizenz
-http://creativecommons.org/licenses/by-sa/3.0/deed.de
+pyLottoSimu
+
+Copyright (C) <2012> Markus Hackspacher
+
+This file is part of pyLottoSimu.
+
+pyLottoverwaltung is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyLottoSimu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyLottoSimu.  If not, see <http://www.gnu.org/licenses/>.
 """
 __doc__ = "The signals for the GUI"
 
@@ -23,7 +39,7 @@ class MeinDialog(QtGui.QMainWindow, Dlg):
    """
    def __init__(self):       
        QtGui.QDialog.__init__(self) 
-       self.setWindowIcon(QtGui.QIcon(join("misc", "icon.ico")))
+       self.setWindowIcon(QtGui.QIcon(join("misc", "pyLottoSimu.svg")))
        self.setupUi(self)
        self.actionLottosim()
        self.timer =  QtCore.QTimer( self )
