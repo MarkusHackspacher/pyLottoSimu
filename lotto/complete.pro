ad94accf212fcd837eff00a490d655dea77db02d
MarkusHackspacher <hackspacher@gmx.de>
2013-07-17 23:54:25 +0200

    language files

---
+++ lotto/complete.pro
@@ -0,0 +1,5 @@
+ SOURCES         = lotto_gui1.py
+ FORMS           = lotto.ui
+ TRANSLATIONS    = lotto1_uk.ts 
+ TRANSLATIONS    = lotto1_de.ts 
+ CODECFORTR      = UTF-8
---
+++ lotto/lotto1_de.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>Info</name>
+    <message>
+        <location filename="lotto_gui1.py" line="228"/>
+        <source>Erstellt mit Python von Markus Hackspacher</source>
+        <translation type="unfinished">Erstellt mit Python von Markus Hackspacher</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="lotto.ui" line="14"/>
+        <source>pyLottoSimu</source>
+        <translation type="unfinished">pyLottoSimu</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="110"/>
+        <source>Lottozahlen</source>
+        <translation type="unfinished">Lottozahlen</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="82"/>
+        <source>Ausgabefeld</source>
+        <translation type="unfinished">Ausgabefeld</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="136"/>
+        <source>Zahlen:</source>
+        <translation type="unfinished">Zahlen:</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="152"/>
+        <source>Anzahl</source>
+        <translation type="unfinished">Anzahl</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="175"/>
+        <source>aus</source>
+        <translation type="unfinished">aus</translation>
+    </message>
+    <message utf8="true">
+        <location filename="lotto.ui" line="191"/>
+        <source>Höchste Zufallszahl</source>
+        <translation type="unfinished">Höchste Zufallszahl</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="228"/>
+        <source>Zufallsgenerator ein</source>
+        <translation type="unfinished">Zufallsgenerator ein</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="244"/>
+        <source>Ausgabefeld leeren</source>
+        <translation type="unfinished">Ausgabefeld leeren</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="362"/>
+        <source>Text</source>
+        <translation type="unfinished">Text</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="289"/>
+        <source>Start</source>
+        <translation type="unfinished">Start</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="305"/>
+        <source>Speed</source>
+        <translation type="unfinished">Geschwindigkeit</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="339"/>
+        <source>Geschwindigkeit</source>
+        <translation type="unfinished">Geschwindigkeit</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="381"/>
+        <source>Datei</source>
+        <translation type="unfinished">Datei</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="436"/>
+        <source>Hilfe</source>
+        <translation type="unfinished">Hilfe</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="393"/>
+        <source>Modus</source>
+        <translation type="unfinished">Modus</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="404"/>
+        <source>Beenden</source>
+        <translation type="unfinished">Beenden</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="409"/>
+        <source>Info</source>
+        <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="420"/>
+        <source>Zufallsgenerator</source>
+        <translation type="unfinished">Zufallsgenerator</translation>
+    </message>
+    <message>
+        <location filename="lotto.ui" line="431"/>
+        <source>Lottosimulation</source>
+        <translation type="unfinished">Lottosimulation</translation>
+    </message>
+</context>
+</TS>
