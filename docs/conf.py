b2194a38462d310a7a0e377609cd41b03218b1b2
MarkusHackspacher <hackspacher@gmx.de>
2015-10-05 21:14:05 +0200

    sphinx mock

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,18 @@
 import sys
 import os
 import shlex
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['PyQt5', 'PyQt4', 'PySide']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
